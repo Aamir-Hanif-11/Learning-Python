10fb6950ec3ac4416a6d221764af1e4e75052c25
Muhammad Amir <aamirhanifkhan@gmail.com>
2024-06-16 14:33:13 +0500

    Just Updated

---
+++ Basic Of Python/FirstProgram.py
@@ -0,0 +1,42 @@
+print("My name is Amir Hanif" , "My age is 49")
+name = "Aamir"
+age = 49
+price = 25.99
+old = True
+a = None
+print(name)
+print("My name is : ", name)
+print(age)
+print(type(a))
+print('Aamir')
+print(age)
+print(5 + 5, "salam", 'kiya howa', 10.22)
+print("Pakistan") 
+name = "Aamir Hanif"
+name = "Aamir"
+print(name)
+a = 2
+b = 5
+sum = a - b
+print(sum)
+a,b=2,3
+txt="@"
+print(2*txt*3)
+A,B="2",3
+Txt="@"
+print((A+Txt)*B)
+A,B="2",3
+Txt="@"
+print((A+Txt)*B)
+A,B=2,3
+C=4
+print(A+B*C)
+A,B=10,5.0
+C=A*B
+print(C)
+A,B=1,2
+C=A/B
+print(C)
+A,B=1.5,3
+C=A//B
+print(C,A/B)
